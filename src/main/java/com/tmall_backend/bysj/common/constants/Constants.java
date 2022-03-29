bca893cadd13f180f4c2328af965c82a245be750
wenshuoliu <wenshuoliu@tencent.com>
2022-03-29 15:13:52 +0800

    fix

--- src/main/java/com/tmall_backend/bysj/common/constants/Constants.java
+++ src/main/java/com/tmall_backend/bysj/common/constants/Constants.java
@@ -5,7 +5,7 @@ package com.tmall_backend.bysj.common.constants;
  * Created on 2022-03-20
  */
 public class Constants {
-    public static final String SESSION_KEY = "current_user_session";
-    public static final String COOKIE_KEY = "current_user_cookie";
+    public static final String SESSION_KEY = "current_customer_session";
+    public static final String COOKIE_KEY = "current_customer_cookie";
     private Constants(){}
 }
